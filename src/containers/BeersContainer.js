eefd14931f35aa9f5df08f4a7c6a36f9140d3c44
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-09 11:45:56 +0000

    API data fetched

--- src/containers/BeersContainer.js
+++ src/containers/BeersContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import BeersList from "../components/BeersList";
 
 const BeersContainer = function () {
+    // useState
+    const [beer, setBeers] = useState([]);
+
+
+    // useEffect - initall set-up
+    useEffect(() => {
+        getBeers(); // array of 80 
+    }, []);
+
+    // API call to get data for useEffect to use
+    const getBeers = async function() {
+        const response = await fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
+        const beers = await response.json();
+        setBeers(beers)
+        console.log(beers)
+    }
+
     return (
         <>
         <h1>BrewDog's HopShop</h1>
-        <BeersList/>
+        <BeersList beers={beers}/>
         </>
     )
 }
